08023acdde2c853515d8913c3a70c39f721288cc
Mahbub <mrmahbuburrahman123@gmail.com>
2025-05-28 01:48:20 +0600

    Updated fetch path and fixed JSON parse issue

--- src/pages/Review/Review.jsx
+++ src/pages/Review/Review.jsx
@@ -11,7 +11,7 @@ const Review = () => {
   const [review, setReview] = useState([]);
 
   useEffect(() => {
-    fetch('../../../public/review.json')
+    fetch('/public/review.json')
       .then(res => res.json())
       .then(data => setReview(data));
   }, []);
